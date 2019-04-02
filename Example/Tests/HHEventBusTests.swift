9c91f1ee6e84d501b7527381d3e014d0080c3cf4
XMFraker <3057600441@qq.com>
2019-04-02 11:32:51 +0800

    add some event bus test cases

---
+++ Example/Tests/HHEventBusTests.swift
@@ -0,0 +1,91 @@
+//
+//  HHEventBusTests.swift
+//  Hermes
+//
+//  Created by XMFraker on 2019/4/1.
+//Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Hermes
+
+
+class Bag {
+    
+}
+
+class HHEventBusTests: QuickSpec {
+    override func spec() {
+
+        
+        describe("basic usage") {
+            
+            it("basic on & automatic off", closure: {
+                
+                var basicCount = 0
+                var basicAutoCount = 0
+                var basicManualCount = 0
+                var bag: Bag? = Bag()
+
+                HHEventBus.on("basic") { _ in
+                    // this should receive over one time
+                    print("here is basic")
+                    basicCount += 1
+                }
+                
+                HHEventBus.on("basic", offBy: bag) { _ in
+                    // this should only receive one time
+                    print("here is basic with automatic remove observer")
+                    basicAutoCount += 1
+                }
+                
+                let observer = HHEventBus.on("basic", handler: { _ in
+                    print("here is basic with manually remove observer")
+                    basicManualCount += 1
+                })
+                
+                HHEventBus.post("basic")
+                
+                bag = nil
+                NotificationCenter.default.removeObserver(observer)
+                HHEventBus.post("basic")
+
+                expect(basicCount).toEventually(equal(2))
+                expect(basicAutoCount).toEventually(equal(1))
+                expect(basicManualCount).toEventually(equal(1))
+            })
+            
+            
+            context("queue", closure: {
+                
+                var queue = 0
+                afterEach {
+                    queue = 0
+                }
+                
+                beforeSuite {
+                    HHEventBus.on("queue", handler: { _ in
+                        print("here is queue")
+                        queue += 1
+                    })
+                }
+                
+                it("test with queue", closure: {
+                    for _ in 0...10 {
+                        HHEventBus.post("queue", style: .whenIdle)
+                    }
+                    expect(queue).toEventually(equal(1))
+                })
+                
+                it("test without queue", closure: {
+                    for _ in 0...10 {
+                        HHEventBus.post("queue")
+                    }
+                    expect(queue).to(equal(11))
+                })
+                
+            })
+        }
+    }
+}
