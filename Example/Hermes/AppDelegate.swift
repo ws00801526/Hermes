58a501064c391d2c5626410cbc5a935b32ae2ac8
XMFraker <3057600441@qq.com>
2019-04-12 10:27:25 +0800

    update module test cases

--- Example/Hermes/AppDelegate.swift
+++ Example/Hermes/AppDelegate.swift
@@ -7,40 +7,80 @@
 //
 
 import UIKit
+import Hermes
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: Hermes.AppDelegate {
 
-    var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
+        
+        let moduleA = ModuleA()
+        let moduleB = ModuleB()
+        
+        // register modules before call super.didFinishLaunching
+        ModuleManager.register(moduleA)
+        ModuleManager.register(moduleB)
+        let boolean =  super.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        // trigger some events
+        ModuleManager.trigger(.doWhat)
+        
+        // unregister some modules
+        ModuleManager.unregister(moduleA)
+        
+        ModuleManager.trigger(.doWhat)
+        
+        
+        print("moduleA \(moduleA) moduleB \(moduleB)")
+        return boolean
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+
+fileprivate extension Event {
+    static let doWhat: Event = Event { (modules, context) -> Any? in
+        modules.forEach {
+            guard let xx = $0 as? NewModule else { return }
+            xx.modDoWhat(context)
+        }
+        return nil
+    }
+}
+
+public protocol NewModule: Module {
+    func modDoWhat(_ context: Context) -> Void
+}
+
+public extension NewModule {
+    func modDoWhat(_ context: Context) -> Void {
+        print("modDoWhat in NewModule extension \(self)")
+    }
+}
+
+class ModuleB: NSObject, Hermes.Module {
+    
+    var priority: Priority { return .high }
+    
+    func modSetup(_ context: Context) {
+        print("mod setup in  \(self)")
+    }
+    
+    func modDoWhat(_ context: Context) {
+         print("modDoWhat in \(self)")
+    }
+}
+
+
+class ModuleA: NSObject, NewModule {
+    
+    var isAsync: Bool { return false }
+    
+    func modSetup(_ context: Context) {
+        print("mod setup in  \(self)")
+    }
+
+    func modDoWhat(_ context: Context) {
+        print("modDoWhat in \(self)")
+    }
+    
+}
